14de07d5f889dbb4306155251bf65de38b49a2e7
smahaduzzaman <smahaduzzaman96@gmail.com>
2023-02-22 10:57:15 +0600

    Third Commit For Creating Sidebar Component

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,7 +1,7 @@
 import { forwardRef } from "react";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { HomeIcon, CreditCardIcon, UserIcon } from "@heroicons/react/24/solid";
+import { useRouter } from "next/router";
 
 const SideBar = forwardRef(({ showNav }, ref) => {
     const router = useRouter();
@@ -16,11 +16,58 @@ const SideBar = forwardRef(({ showNav }, ref) => {
                         alt="company logo"
                     />
                 </picture>
+          </div>
+
+          <div className="flex flex-col">
+              <Link href="/">
+                  <div
+                      className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${router.pathname == "/"
+                              ? "bg-orange-100 text-orange-500"
+                              : "text-gray-400 hover:bg-orange-100 hover:text-orange-500"
+                          }`}
+                  >
+                      <div className="mr-2">
+                          <HomeIcon className="h-5 w-5" />
+                      </div>
+                      <div>
+                          <p>Home</p>
+                      </div>
+                  </div>
+              </Link>
+              <Link href="/account">
+                  <div
+                      className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${router.pathname == "/account"
+                              ? "bg-orange-100 text-orange-500"
+                              : "text-gray-400 hover:bg-orange-100 hover:text-orange-500"
+                          }`}
+                  >
+                      <div className="mr-2">
+                          <UserIcon className="h-5 w-5" />
+            </div>
+                      <div>
+                          <p>Account</p>
+                      </div>
+                    </div>
+                </Link>
+                <Link href="/billing">
+                    <div
+                        className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${router.pathname == "/billing"
+                                ? "bg-orange-100 text-orange-500"
+                                : "text-gray-400 hover:bg-orange-100 hover:text-orange-500"
+                            }`}
+                    >
+                        <div className="mr-2">
+                            <CreditCardIcon className="h-5 w-5" />
+                        </div>
+                        <div>
+                            <p>Billing</p>
+                        </div>
+                    </div>
+                </Link>
             </div>
         </div>
-    )
-
-})
+    );
+});
 
 SideBar.displayName = "SideBar";
 
